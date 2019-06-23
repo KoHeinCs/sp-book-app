5de64601b590acc53387acbc546d522d261500d6
Hein Htet Aung <heinhtet247232@gmail.com>
2019-06-23 10:01:53 +0630

    first commit

---
+++ src/main/java/com/zawmoehtike/spbookstoreapp/controller/BookController.java
@@ -0,0 +1,24 @@
+package com.zawmoehtike.spbookstoreapp.controller;
+
+import com.zawmoehtike.spbookstoreapp.domain.Book;
+import com.zawmoehtike.spbookstoreapp.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/book")
+public class BookController {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+}
